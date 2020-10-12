6f062be7ae7b4c86ad71eecd248cd436311dba8e
IDilantha <isurudilantha@gmail.com>
2020-10-13 00:11:39 +0530

    Added the firebase initialization

--- src/App.js
+++ src/App.js
@@ -4,14 +4,28 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth'
 
-import {useAuth} from 'react-firebase-hooks/auth';
-import {useCollectionData} from 'react-firebase-hooks/firestore';
+import { useAuth } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDr_mMMJNASrd4r8DuVRrFeYrv_738PFh0",
+  authDomain: "superchat-4f544.firebaseapp.com",
+  databaseURL: "https://superchat-4f544.firebaseio.com",
+  projectId: "superchat-4f544",
+  storageBucket: "superchat-4f544.appspot.com",
+  messagingSenderId: "1051060596746",
+  appId: "1:1051060596746:web:4d48d4909d462496582ff3",
+  measurementId: "G-E1JTWFG4KM"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+
       </header>
     </div>
   );
